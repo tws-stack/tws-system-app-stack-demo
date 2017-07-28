850ad8e78b022c05c1e808d9d97df1c1d5898d5c
hhu <hhu@thoughtworks.com>
2017-07-28 13:41:02 +0800

    init core-service project

---
+++ modules/core-service/src/main/java/school/thoughtworks/system/app/demo/service/core/CoreApplication.java
@@ -0,0 +1,12 @@
+package school.thoughtworks.system.app.demo.service.core;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoreApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CoreApplication.class, args);
+    }
+}
