f30d2db717b28733fea4bcdf98204916b2bdeac6
zhangpei <pezhang@thoughtworks.com>
2017-08-03 14:13:54 +0800

    zhangpei - core service - add a eureka client annotation on main class

--- modules/core-service/src/main/java/school/thoughtworks/system/app/demo/service/core/CoreApplication.java
+++ modules/core-service/src/main/java/school/thoughtworks/system/app/demo/service/core/CoreApplication.java
@@ -2,10 +2,11 @@ package school.thoughtworks.system.app.demo.service.core;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
+@EnableEurekaClient
 @SpringBootApplication
 public class CoreApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(CoreApplication.class, args);
     }
