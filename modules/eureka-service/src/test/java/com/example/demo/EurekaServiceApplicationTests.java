5b2d7b3812bea1389b3779b3e42bb92b7fa0f1c8
zhangpei <pezhang@thoughtworks.com>
2017-08-03 12:09:45 +0800

    zhangpei - eureka service - create test for eureka service

--- modules/eureka-service/src/test/java/com/example/demo/EurekaServiceApplicationTests.java
+++ modules/eureka-service/src/test/java/com/example/demo/EurekaServiceApplicationTests.java
@@ -2,15 +2,25 @@ package com.example.demo;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import static org.assertj.core.api.Assertions.*;
+
 @RunWith(SpringRunner.class)
-@SpringBootTest
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 public class EurekaServiceApplicationTests {
+	@Autowired
+	TestRestTemplate testRestTemplate;
 
 	@Test
-	public void contextLoads() {
+	public void start_eureka_service_should_return_status_OK() {
+		ResponseEntity<String> responseEntity = testRestTemplate.getForEntity("/eureka/apps",String.class);
+		assertThat(responseEntity.getStatusCode()).isEqualTo(HttpStatus.OK);
 	}
 
 }
