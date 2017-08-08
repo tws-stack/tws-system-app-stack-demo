6420a49dae30664ca72e5bc61db7913f7c3eb442
zhangpei <pezhang@thoughtworks.com>
2017-08-08 09:53:39 +0800

    zhangpei - web - create compute service web

--- modules/web/src/component/compute-service/ComputeApp.js
+++ modules/web/src/component/compute-service/ComputeApp.js
@@ -1,37 +1,85 @@
 import React, {Component} from "react";
 import request from 'superagent';
-require('../style/App.css');
+require('../../style/App.css');
+
 
 class ComputeApp extends Component {
-  constructor(props){
+  constructor(props) {
     super(props);
-    this.state={
-      text: ""
+    this.focus = (name) => {
+      this.focus(name)
+    };
+    this.state = {
+      text: "",
+      answerAvailable: false,
+      result: ''
     }
   }
 
-  componentDidMount(){
-    request
-      .get('/api/hello_world_service/hello')
-      .end((err,res)=>{
-        if(err){
-          return console.log('err is :',err);
-        }
+  componentDidMount() {
+    this.focus("add_a");
+  }
 
-        this.setState({
-          text:res.text
-        });
+  focus(name) {
+    this[name].focus();
+  }
+
+  handleClick() {
+    request
+      .get('/api/compute_service/compute')
+      .query({a: this.add_a})
+      .query({b: this.add_b})
+      .end((err, res) => {
+        if (err) {
+          console.log("err is: ", err);
+        } else {
+          this.setState({
+            answerAvailable: true,
+            result: res.text
+          })
+        }
       })
+  }
+
+  handleFocus(){
+    this.setState({
+      answerAvailable: false
+    })
   }
 
   render() {
     return (
-      <div >
-        <h1>{this.state.text}</h1>
+      <div>
+        <form>
+          <div className="form-group">
+            <label htmlFor="formGroupExampleInput">一个加数</label>
+            <input type="number" className="form-control" id="formGroupExampleInput" placeholder="Example input"
+                   ref={(ref) => {
+                     this.add_a = ref;
+                   }} onBlur={() => {
+              this.focus("add_b")
+            }} onFocus={()=>{this.handleFocus()}} />
+          </div>
+          <div className="form-group">
+            <label htmlFor="formGroupExampleInput2">另一个加数</label>
+            <input type="number" className="form-control" id="formGroupExampleInput2" placeholder="Another input"
+                   ref={(ref) => {
+                     this.add_b = ref;
+                   }}/>
+          </div>
+          <input className="btn btn-primary" type="submit" value="求和" onClick={() => {
+            this.handleClick()
+          }}/>
+        </form>
+        { this.state.answerAvailable ?
+          (<div className="alert alert-success" role="alert">
+            <strong>Well done!</strong> You successfully get answer :<a href="#" className="alert-link">{this.state.result}</a>.
+          </div>) : ""
+        }
       </div>
     );
   }
 }
 
-export default App;
+export default ComputeApp;
 
--- modules/web/src/index.js
+++ modules/web/src/index.js
@@ -1,10 +1,29 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './component/App';
-import { Router, Route, hashHistory, IndexRoute } from 'react-router';
+import {
+  BrowserRouter as Router,
+  Route,
+  Link,
+  IndexRoute
+} from 'react-router-dom'
+
+import ComputeApp from './component/compute-service/ComputeApp';
 
 ReactDOM.render(
-  <App />,
+  <Router>
+    <div>
+      <div>
+        <Route path="/hello" component={App}/>
+        <Route path="/compute" component={ComputeApp}/>
+      </div>
+
+      <ul>
+        <li><Link to="/hello">hello world!</Link></li>
+        <li><Link to="/compute">compute App</Link></li>
+      </ul>
+    </div>
+  </Router>,
   document.getElementById('root'));
 
-// ReactDOM.render(<App />, document.getElementById('root'));
+
