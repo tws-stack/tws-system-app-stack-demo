bd419469bc1923b8df081c963c1eb96c6671fe7a
zhangpei <pezhang@thoughtworks.com>
2017-08-09 14:00:01 +0800

    zhangpei - react dev - complete component test

--- modules/web-dev/src/test/App.test.js
+++ modules/web-dev/src/test/App.test.js
@@ -2,8 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from '../component/App';
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
+describe('test for components',()=>{
+  it('test renders without crashing',()=>{
+    const div = document.createElement('div');
+      ReactDOM.render(<App />,div);
+  })
 });
-
